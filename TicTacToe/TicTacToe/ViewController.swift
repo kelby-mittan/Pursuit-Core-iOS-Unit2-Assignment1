071720fd45b22c0cc8abe5bd812a9ea6fbc3dc13
Kelby Mittan <kelbymittan@pursuit.org>
2019-10-31 15:02:02 -0400

    made a startGame button and added a function to either put an x or o in the 9 buttons

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,23 +13,60 @@ class ViewController: UIViewController {
     
     @IBOutlet var xorOButton: [UIButton]!
     
+    @IBOutlet weak var letsPlay: UILabel!
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+        for button in xorOButton {
+            button.isEnabled = false
+        }
         
   }
-
+    @IBAction func letsPlayGame(_ sender: UIButton) {
+        for button in xorOButton {
+            button.isEnabled = true
+        }
+    }
+    
+    var playerOneGo = true
+    
+    func pickXorO(_ button: UIButton) {
+        if playerOneGo {
+            button.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            button.isEnabled = false
+            playerOneGo.toggle()
+        } else {
+            button.setBackgroundImage(UIImage(named: "o"), for: .normal)
+            button.isEnabled = false
+            playerOneGo.toggle()
+        }
+    }
+    
     @IBAction func selectedButton(_ sender: UIButton) {
         
-//        for (index, button) in xorOButton.enumerated() {
-//        if sender.tag == index {
-//          button.setBackgroundImage(UIImage(named: "x"), for: .normal)
-//        }
+        
         switch sender.tag {
         case 0:
-            sender.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            pickXorO(sender)
+        case 1:
+            pickXorO(sender)
+        case 2:
+            pickXorO(sender)
+        case 3:
+            pickXorO(sender)
+        case 4:
+            pickXorO(sender)
+        case 5:
+            pickXorO(sender)
+        case 6:
+            pickXorO(sender)
+        case 7:
+            pickXorO(sender)
+        case 8:
+            pickXorO(sender)
         default:
-            xorOButton[1].setBackgroundImage(UIImage(named: "x"), for: .normal)
+            pickXorO(sender)
         }
         
         
