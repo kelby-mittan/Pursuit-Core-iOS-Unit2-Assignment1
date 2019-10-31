4849de1a994c667d131cccd9a5bf73d46ccbd6d8
Kelby Mittan <kelbymittan@pursuit.org>
2019-10-31 17:09:19 -0400

    added sets for taken squares

--- TicTacToe/TicTacToe/GameButton.swift
+++ TicTacToe/TicTacToe/GameButton.swift
@@ -11,4 +11,5 @@ import UIKit
 class GameButton: UIButton {
   @IBInspectable var row: Int = 0
   @IBInspectable var col: Int = 0
+    
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     
     @IBOutlet var xorOButton: [UIButton]!
     
-    @IBOutlet weak var letsPlay: UILabel!
+    @IBOutlet weak var playerTurn: UILabel!
+    @IBOutlet weak var playerTwoTurn: UILabel!
     
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,6 +28,19 @@ class ViewController: UIViewController {
         for button in xorOButton {
             button.isEnabled = true
         }
+        playerTurn.text = "Player One.... Go!!"
+    }
+    
+    func insertXorO(_ button: UIButton) -> String {
+        var xOrO: String
+        if playerOneGo {
+            xOrO = "o"
+        } else if playerOneGo == false {
+            xOrO = "x"
+        } else {
+            xOrO = ""
+        }
+        return xOrO
     }
     
     var playerOneGo = true
@@ -36,38 +50,70 @@ class ViewController: UIViewController {
             button.setBackgroundImage(UIImage(named: "x"), for: .normal)
             button.isEnabled = false
             playerOneGo.toggle()
+            playerTwoTurn.text = "Player Two.... Go!!"
+            playerTurn.text = ""
         } else {
             button.setBackgroundImage(UIImage(named: "o"), for: .normal)
             button.isEnabled = false
             playerOneGo.toggle()
+            playerTurn.text = "Player One.... Go!!"
+            playerTwoTurn.text = ""
         }
     }
     
+    var collectionOfXO = RowColumnSet()
+    
     @IBAction func selectedButton(_ sender: UIButton) {
-        
         
         switch sender.tag {
         case 0:
             pickXorO(sender)
+            collectionOfXO.rowOne.insert(insertXorO(sender))
+            collectionOfXO.columnOne.insert(insertXorO(sender))
+            collectionOfXO.diagOne.insert(insertXorO(sender))
         case 1:
             pickXorO(sender)
+            collectionOfXO.rowOne.insert(insertXorO(sender))
+            collectionOfXO.columnTwo.insert(insertXorO(sender))
         case 2:
             pickXorO(sender)
+            collectionOfXO.rowOne.insert(insertXorO(sender))
+            collectionOfXO.columnThree.insert(insertXorO(sender))
+            collectionOfXO.diagTwo.insert(insertXorO(sender))
         case 3:
             pickXorO(sender)
+            collectionOfXO.rowTwo.insert(insertXorO(sender))
+            collectionOfXO.columnOne.insert(insertXorO(sender))
         case 4:
             pickXorO(sender)
+            collectionOfXO.rowTwo.insert(insertXorO(sender))
+            collectionOfXO.columnTwo.insert(insertXorO(sender))
+            collectionOfXO.diagOne.insert(insertXorO(sender))
+            collectionOfXO.diagTwo.insert(insertXorO(sender))
         case 5:
             pickXorO(sender)
+            collectionOfXO.rowTwo.insert(insertXorO(sender))
+            collectionOfXO.columnThree.insert(insertXorO(sender))
         case 6:
             pickXorO(sender)
+            collectionOfXO.rowThree.insert(insertXorO(sender))
+            collectionOfXO.columnOne.insert(insertXorO(sender))
+            collectionOfXO.diagTwo.insert(insertXorO(sender))
         case 7:
             pickXorO(sender)
+            collectionOfXO.rowThree.insert(insertXorO(sender))
+            collectionOfXO.columnTwo.insert(insertXorO(sender))
         case 8:
             pickXorO(sender)
+            collectionOfXO.rowThree.insert(insertXorO(sender))
+            collectionOfXO.columnThree.insert(insertXorO(sender))
+            collectionOfXO.diagOne.insert(insertXorO(sender))
         default:
             pickXorO(sender)
         }
+//        print(collectionOfXO.rowOne)
+//        print(collectionOfXO.rowTwo)
+        print(collectionOfXO.columnTwo)
         
         
     }
