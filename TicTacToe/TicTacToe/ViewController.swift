83e695784337e5539656797a03a44cf9eea1420c
Kelby Mittan <kelbymittan@pursuit.org>
2019-10-31 14:14:37 -0400

    set up rows and columns in main storyboard

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+    
+    
+    @IBOutlet var xorOButton: [UIButton]!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+        
   }
 
-
+    @IBAction func selectedButton(_ sender: UIButton) {
+        
+//        for (index, button) in xorOButton.enumerated() {
+//        if sender.tag == index {
+//          button.setBackgroundImage(UIImage(named: "x"), for: .normal)
+//        }
+        switch sender.tag {
+        case 0:
+            sender.setBackgroundImage(UIImage(named: "x"), for: .normal)
+        default:
+            xorOButton[1].setBackgroundImage(UIImage(named: "x"), for: .normal)
+        }
+        
+        
+    }
+    
 }
 
