317cdcfa6983ded61532b6199649bd7f50821b95
Kelby Mittan <kelbymittan@pursuit.org>
2019-11-01 13:59:15 -0400

    added a playVsComp button, still need to do some debugging and major refactoring

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,31 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class TicTacToeBrain {
-  
+    var collectionOfXO = RowColumnArr()
+    var playerOneGo = true
+    let xArray: Array = ["x","x","x"]
+    let oArray: Array = ["o","o","o"]
+    
+    func appendXorO(_ button: UIButton) -> String {
+        var xOrO: String
+        if playerOneGo {
+            xOrO = "o"
+        } else if playerOneGo == false {
+            xOrO = "x"
+        } else {
+            xOrO = ""
+        }
+        return xOrO
+    }
+    
+//    func appendXorOForComp(_ button: UIButton) -> String {
+//        var xOrO: String
+//        if playerOneGo {
+//            xOrO = "o"
+//        }
+//    }
+    
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var playerTwoTurn: UILabel!
     @IBOutlet weak var winnerLabel: UILabel!
     
+    var collectionOfXO = RowColumnArr()
+    let xArray: Array = ["x","x","x"]
+    let oArray: Array = ["o","o","o"]
+    var twoPlayerBool = Bool()
+    var playVsCompBool = Bool()
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -25,7 +31,18 @@ class ViewController: UIViewController {
         }
         
   }
-    @IBAction func letsPlayGame(_ sender: UIButton) {
+    @IBAction func twoPlayerGame(_ sender: UIButton) {
+        for button in xorOButton {
+            button.isEnabled = true
+        }
+        playerTurn.text = "Player One.... Go!!"
+        twoPlayerBool = true
+        playVsCompBool = false
+    }
+    
+    @IBAction func playVsComp(_ sender: UIButton) {
+        playVsCompBool = true
+        twoPlayerBool = false
         for button in xorOButton {
             button.isEnabled = true
         }
@@ -82,107 +99,192 @@ class ViewController: UIViewController {
         }
     }
     
-    var collectionOfXO = RowColumnArr()
-    let xArray: Array = ["x","x","x"]
-    let oArray: Array = ["o","o","o"]
-    
-    @IBAction func selectedButton(_ sender: UIButton) {
-        
-        switch sender.tag {
-        case 0:
-            pickXorO(sender)
-            collectionOfXO.rowOne.append(appendXorO(sender))
-            collectionOfXO.columnOne.append(appendXorO(sender))
-            collectionOfXO.diagOne.append(appendXorO(sender))
-        case 1:
-            pickXorO(sender)
-            collectionOfXO.rowOne.append(appendXorO(sender))
-            collectionOfXO.columnTwo.append(appendXorO(sender))
-        case 2:
-            pickXorO(sender)
-            collectionOfXO.rowOne.append(appendXorO(sender))
-            collectionOfXO.columnThree.append(appendXorO(sender))
-            collectionOfXO.diagTwo.append(appendXorO(sender))
-        case 3:
-            pickXorO(sender)
-            collectionOfXO.rowTwo.append(appendXorO(sender))
-            collectionOfXO.columnOne.append(appendXorO(sender))
-        case 4:
-            pickXorO(sender)
-            collectionOfXO.rowTwo.append(appendXorO(sender))
-            collectionOfXO.columnTwo.append(appendXorO(sender))
-            collectionOfXO.diagOne.append(appendXorO(sender))
-            collectionOfXO.diagTwo.append(appendXorO(sender))
-        case 5:
-            pickXorO(sender)
-            collectionOfXO.rowTwo.append(appendXorO(sender))
-            collectionOfXO.columnThree.append(appendXorO(sender))
-        case 6:
-            pickXorO(sender)
-            collectionOfXO.rowThree.append(appendXorO(sender))
-            collectionOfXO.columnOne.append(appendXorO(sender))
-            collectionOfXO.diagTwo.append(appendXorO(sender))
-        case 7:
-            pickXorO(sender)
-            collectionOfXO.rowThree.append(appendXorO(sender))
-            collectionOfXO.columnTwo.append(appendXorO(sender))
-        case 8:
-            pickXorO(sender)
-            collectionOfXO.rowThree.append(appendXorO(sender))
-            collectionOfXO.columnThree.append(appendXorO(sender))
-            collectionOfXO.diagOne.append(appendXorO(sender))
-        default:
-            pickXorO(sender)
-        }
-        
-        if collectionOfXO.rowOne == xArray {
-            oneWins()
-        } else if collectionOfXO.rowOne == oArray {
-            twoWins()
-        } else if collectionOfXO.rowTwo == xArray {
-            oneWins()
-        } else if collectionOfXO.rowTwo == oArray {
-            twoWins()
-        } else if collectionOfXO.rowThree == xArray {
-            oneWins()
-        } else if collectionOfXO.rowThree == oArray {
-            twoWins()
-        } else if collectionOfXO.columnOne == xArray {
-            oneWins()
-        } else if collectionOfXO.columnOne == oArray {
-            twoWins()
-        } else if collectionOfXO.columnTwo == xArray {
-            oneWins()
-        } else if collectionOfXO.columnTwo == oArray {
-            twoWins()
-        } else if collectionOfXO.columnThree == xArray {
-            oneWins()
-        } else if collectionOfXO.columnThree == oArray {
-            twoWins()
-        } else if collectionOfXO.diagOne == xArray {
-            oneWins()
-        } else if collectionOfXO.diagOne == oArray {
-            twoWins()
-        } else if collectionOfXO.diagTwo == xArray {
-            oneWins()
-        } else if collectionOfXO.diagTwo == oArray {
-            twoWins()
-        }
-        
-        
-
-        
-        print("row1\(collectionOfXO.rowOne)")
-        print(collectionOfXO.rowTwo)
-        print(collectionOfXO.rowThree)
-        print("col1 \(collectionOfXO.columnOne)")
-        print(collectionOfXO.columnTwo)
-        print(collectionOfXO.columnThree)
-        print("diag1\(collectionOfXO.diagOne)")
-        print(collectionOfXO.diagTwo)
+    func pickXCompO(_ button: UIButton) {
+        let randomButton = xorOButton.randomElement()!
+            button.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            button.isEnabled = false
+            playerTurn.text = ""
+            randomButton.setBackgroundImage(UIImage(named: "o"), for: .normal)
+            randomButton.isEnabled = false
+            xorOButton.removeAll { (button) -> Bool in
+                button.isEnabled == false            
+            
+        } //else {
+//            xorOButton.randomElement()?.setBackgroundImage(UIImage(named: "o"), for: .normal)
+//            button.isEnabled = false
+//            playerOneGo.toggle()
+//            playerTurn.text = "Player One.... Go!!"
+//            playerTwoTurn.text = ""
+//        }
     }
     
+    func allButtonsDisabled() -> Bool {
+        var disabledButtonArr = [Bool]()
+        var itsATie = Bool()
+        for button in xorOButton {
+            if button.isEnabled == false {
+                disabledButtonArr.append(true)
+            }
+        }
+        if disabledButtonArr.count == 9 {
+            itsATie = true
+        }
+        return itsATie
+    }
+    
+    
+    @IBAction func selectedButton(_ sender: UIButton) {
+      
+        if twoPlayerBool {
+            switch sender.tag {
+            case 0:
+                pickXorO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+            case 1:
+                pickXorO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+            case 2:
+                pickXorO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 3:
+                pickXorO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+            case 4:
+                pickXorO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 5:
+                pickXorO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+            case 6:
+                pickXorO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 7:
+                pickXorO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+            case 8:
+                pickXorO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+            default:
+                pickXorO(sender)
+            }
+            
+            if collectionOfXO.rowOne == xArray {
+                oneWins()
+            } else if collectionOfXO.rowOne == oArray {
+                twoWins()
+            } else if collectionOfXO.rowTwo == xArray {
+                oneWins()
+            } else if collectionOfXO.rowTwo == oArray {
+                twoWins()
+            } else if collectionOfXO.rowThree == xArray {
+                oneWins()
+            } else if collectionOfXO.rowThree == oArray {
+                twoWins()
+            } else if collectionOfXO.columnOne == xArray {
+                oneWins()
+            } else if collectionOfXO.columnOne == oArray {
+                twoWins()
+            } else if collectionOfXO.columnTwo == xArray {
+                oneWins()
+            } else if collectionOfXO.columnTwo == oArray {
+                twoWins()
+            } else if collectionOfXO.columnThree == xArray {
+                oneWins()
+            } else if collectionOfXO.columnThree == oArray {
+                twoWins()
+            } else if collectionOfXO.diagOne == xArray {
+                oneWins()
+            } else if collectionOfXO.diagOne == oArray {
+                twoWins()
+            } else if collectionOfXO.diagTwo == xArray {
+                oneWins()
+            } else if collectionOfXO.diagTwo == oArray {
+                twoWins()
+            } else if allButtonsDisabled() {
+                winnerLabel.text = "It's a tie!!"
+                playerTurn.text = ""
+                playerTwoTurn.text = ""
+            }
+        }
+        
+        if playVsCompBool {
+            switch sender.tag {
+            case 0:
+                pickXCompO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+            case 1:
+                pickXCompO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+            case 2:
+                pickXCompO(sender)
+                collectionOfXO.rowOne.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 3:
+                pickXCompO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+            case 4:
+                pickXCompO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 5:
+                pickXCompO(sender)
+                collectionOfXO.rowTwo.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+            case 6:
+                pickXCompO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnOne.append(appendXorO(sender))
+                collectionOfXO.diagTwo.append(appendXorO(sender))
+            case 7:
+                pickXCompO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnTwo.append(appendXorO(sender))
+            case 8:
+                pickXCompO(sender)
+                collectionOfXO.rowThree.append(appendXorO(sender))
+                collectionOfXO.columnThree.append(appendXorO(sender))
+                collectionOfXO.diagOne.append(appendXorO(sender))
+            default:
+                pickXCompO(sender)
+            }
+            
+        }
+        
+//        print("row1\(collectionOfXO.rowOne)")
+//        print(collectionOfXO.rowTwo)
+//        print(collectionOfXO.rowThree)
+//        print("col1 \(collectionOfXO.columnOne)")
+//        print(collectionOfXO.columnTwo)
+//        print(collectionOfXO.columnThree)
+//        print("diag1\(collectionOfXO.diagOne)")
+//        print(collectionOfXO.diagTwo)
+    }
+    @IBOutlet var resetButtons: [UIButton]!
+    
     @IBAction func reset(_ sender: UIButton) {
+        xorOButton = resetButtons
         for button in xorOButton {
             button.setBackgroundImage(UIImage(named: "defaultGrey"), for: .normal)
             button.isEnabled = true
